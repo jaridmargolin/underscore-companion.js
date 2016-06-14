dbcf0798f879ea5b8ebb888558f7da51c0d5f3a1
Jarid Margolin <jaridmargolin@gmail.com>
2016-06-13 21:16:21 -0700

    Release v0.1.0

---
+++ dist/common/underscore-companion.js
@@ -0,0 +1,24 @@
+/*!
+ * underscore-companion.js
+ * 
+ * Copyright (c) 2014
+ */
+
+
+
+
+/* -----------------------------------------------------------------------------
+ * underscore-companion
+ * ---------------------------------------------------------------------------*/
+
+module.exports = {
+  bindPrototypes : require('./bindPrototypes'),
+  decorate       : require('./decorate'),
+  deepMerge      : require('./deepMerge'),
+  delayApply     : require('./delayApply'),
+  delayCall      : require('./delayCall'),
+  encase         : require('./encase'),
+  evaluate       : require('./evaluate')
+};
+
+
